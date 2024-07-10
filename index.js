5231e4266a7fcbaf6cf4a874994ce61662a03696
Diego <diegosebastianorona@gmail.com>
2024-07-10 16:03:18 -0300

    Corrigiendo PORT

--- index.js
+++ index.js
@@ -4,6 +4,4 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Deploy");
 });
-const PORT = 3000;
-
-app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+const PORT = process.env.PORT || 3000
